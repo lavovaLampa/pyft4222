d0b96e08a8aff1cfa5a73d15a72109cf5e545326
Martin Cizmar (LavovaLampa) <betacheer@gmail.com>
2021-07-11 01:49:13 +0200

    Refactor ftd2xx_wrapper

--- ftd2xx_wrapper.py
+++ ftd2xx_wrapper.py
@@ -12,20 +12,55 @@ except OSError as e:
     print("Unable to load shared library!")
     exit(1)
 
+# DLL function protoypes declaration
 
-_create_device_info_list = ftlib.FT_CreateDeviceInfo_list
+_create_device_info_list = ftlib.FT_CreateDeviceInfoList
 _create_device_info_list.argtypes = [POINTER(c_uint)]
 _create_device_info_list.restype = FTStatus
 
+_get_device_info_list = ftlib.FT_GetDeviceInfoList
+_get_device_info_list.argtypes = []
+_get_device_info_list.restype = FTStatus
 
-def create_device_info_list() -> int:
-    dev_count = c_uint()
-    result: FTStatus = _create_device_info_list(byref(dev_count))
+_get_device_info_detail = ftlib.FT_GetDeviceInfoDetail
+_get_device_info_detail.argtypes = []
+_get_device_info_detail.restype = FTStatus
 
-    if result != FTStatus.OK:
-        raise RuntimeError("TODO")
+_open = ftlib.FT_Open
+_open.argtypes = [c_int, POINTER(c_void_p)]
+_open.restype = FTStatus
 
-    return dev_count.value
+_open_ex = ftlib.FT_OpenEx
+_open_ex.argtypes = [c_void_p, c_uint, POINTER(c_void_p)]
+_open_ex.restype = FTStatus
+
+_close = ftlib.FT_Close
+_close.argtypes = [c_void_p]
+_close.restype = FTStatus
+
+_get_device_info = ftlib.FT_GetDeviceInfo
+_get_device_info.argtypes = [c_void_p, POINTER(
+    c_uint), POINTER(c_uint), c_char_p, c_char_p, c_void_p]
+_get_device_info.restype = FTStatus
+
+_get_driver_version = ftlib.FT_GetDriverVersion
+_get_driver_version.argtypes = [c_void_p, POINTER(c_uint)]
+_get_driver_version.restype = FTStatus
+
+_purge = ftlib.FT_Purge
+_purge.argtypes = [c_void_p, c_uint]
+_purge.restype = FTStatus
+
+_reset_device = ftlib.FT_ResetDevice
+_reset_device.argtypes = [c_void_p]
+_reset_device.restype = FTStatus
+
+# Constants
+
+DESCRIPTION_MAX_LEN: Final[int] = 64
+SERIAL_NUMBER_MAX_LEN: Final[int] = 16
+
+# Data types
 
 
 class DeviceInfoListNode(NamedTuple):
@@ -38,27 +73,60 @@ class DeviceInfoListNode(NamedTuple):
     handle: FtHandle
 
 
-_get_device_info_list = ftlib.FT_GetDeviceInfoList
-_get_device_info_list.argtypes = []
-_get_device_info_list.restype = FTStatus
+class _OpenExFlag(IntEnum):
+    BY_SERIAL_NUMBER = 1
+    BY_DESCRIPTION = 2
+    BY_LOCATION = 4
+
+
+class PurgeType(IntFlag):
+    RX = 1
+    TX = 2
+
+
+class FtDeviceType(IntEnum):
+    DEV_BM = 0
+    DEV_AM = auto()
+    DEV_100AX = auto()
+    DEV_UNKNOWN = auto()
+    DEV_2232C = auto()
+    DEV_232R = auto()
+    DEV_2232H = auto()
+    DEV_4232H = auto()
+    DEV_232H = auto()
+    DEV_X_SERIES = auto()
+    DEV_4222H_0 = auto()
+    DEV_4222H_1_2 = auto()
+    DEV_4222H_3 = auto()
+    DEV_4222_PROG = auto()
+    DEV_900 = auto()
+    DEV_930 = auto()
+    DEV_UMFTPD3A = auto()
+
+
+class DeviceInfo(NamedTuple):
+    dev_type: FtDeviceType
+    dev_id: int
+    serial_number: str
+    description: str
+
+
+def create_device_info_list() -> int:
+    dev_count = c_uint()
+    result: FTStatus = _create_device_info_list(byref(dev_count))
+
+    if result != FTStatus.OK:
+        raise RuntimeError("TODO")
+
+    return dev_count.value
 
 
 def get_device_info_list() -> None:
     pass
 
 
-_get_device_info_detail = ftlib.FT_GetDeviceInfoDetail
-_get_device_info_detail.argtypes = []
-_get_device_info_detail.restype = FTStatus
-
-
 def get_device_info_detail() -> None:
     pass
-
-
-_open = ftlib.FT_Open
-_open.argtypes = [c_int, POINTER(c_void_p)]
-_open.restype = FTStatus
 
 
 def open(dev_id: int) -> Optional[FtHandle]:
@@ -69,17 +137,6 @@ def open(dev_id: int) -> Optional[FtHandle]:
         raise RuntimeError("TODO")
 
     return FtHandle(ft_handle)
-
-
-class _OpenExFlag(IntEnum):
-    BY_SERIAL_NUMBER = 1
-    BY_DESCRIPTION = 2
-    BY_LOCATION = 4
-
-
-_open_ex = ftlib.FT_OpenEx
-_open_ex.argtypes = [c_void_p, c_uint, POINTER(c_void_p)]
-_open_ex.restype = FTStatus
 
 
 def open_by_serial(serial_num: str) -> Optional[FtHandle]:
@@ -124,50 +181,8 @@ def open_by_location(location: int) -> Optional[FtHandle]:
     return FtHandle(ft_handle)
 
 
-_close = ftlib.FT_Close
-_close.argtypes = [c_void_p]
-_close.restype = FTStatus
-
-
 def close(ft_handle: FtHandle) -> None:
     pass
-
-
-class FtDeviceType(IntEnum):
-    DEV_BM = 0
-    DEV_AM = auto()
-    DEV_100AX = auto()
-    DEV_UNKNOWN = auto()
-    DEV_2232C = auto()
-    DEV_232R = auto()
-    DEV_2232H = auto()
-    DEV_4232H = auto()
-    DEV_232H = auto()
-    DEV_X_SERIES = auto()
-    DEV_4222H_0 = auto()
-    DEV_4222H_1_2 = auto()
-    DEV_4222H_3 = auto()
-    DEV_4222_PROG = auto()
-    DEV_900 = auto()
-    DEV_930 = auto()
-    DEV_UMFTPD3A = auto()
-
-
-class DeviceInfo(NamedTuple):
-    dev_type: FtDeviceType
-    dev_id: int
-    serial_number: str
-    description: str
-
-
-DESCRIPTION_MAX_LEN: Final[int] = 64
-SERIAL_NUMBER_MAX_LEN: Final[int] = 16
-
-
-_get_device_info = ftlib.FT_GetDeviceInfo
-_get_device_info.argtypes = [c_void_p, POINTER(
-    c_uint), POINTER(c_uint), c_char_p, c_char_p, c_void_p]
-_get_device_info.restype = FTStatus
 
 
 def get_device_info(ft_handle: FtHandle, ) -> DeviceInfo:
@@ -196,11 +211,6 @@ def get_device_info(ft_handle: FtHandle, ) -> DeviceInfo:
     )
 
 
-_get_driver_version = ftlib.FT_GetDriverVersion
-_get_driver_version.argtypes = [c_void_p, POINTER(c_uint)]
-_get_driver_version.restype = FTStatus
-
-
 def get_driver_version(ft_handle: FtHandle) -> int:
     driver_version = c_uint()
 
@@ -212,26 +222,11 @@ def get_driver_version(ft_handle: FtHandle) -> int:
     return driver_version.value
 
 
-class PurgeType(IntFlag):
-    RX = 1
-    TX = 2
-
-
-_purge = ftlib.FT_Purge
-_purge.argtypes = [c_void_p, c_uint]
-_purge.restype = FTStatus
-
-
 def purge(ft_handle: FtHandle, purge_type_mask: PurgeType) -> None:
     result: FTStatus = _purge(FtHandle, purge_type_mask)
 
     if result != FTStatus.OK:
         raise RuntimeError("TODO")
-
-
-_reset_device = ftlib.FT_ResetDevice
-_reset_device.argtypes = [c_void_p]
-_reset_device.restype = FTStatus
 
 
 def reset_device(ft_handle: FtHandle) -> None:
