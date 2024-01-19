d2150bdf5144a3be58b6871f87c26d807d729ed8
Martin Cizmar (LavovaLampa) <betacheer@gmail.com>
2024-01-19 21:35:21 +0100

    Bump version: 0.1.2 → 0.2.0

--- src/pyft4222/__init__.py
+++ src/pyft4222/__init__.py
@@ -13,7 +13,7 @@ from pyft4222.wrapper import ftd2xx as ftd
 from pyft4222.wrapper import gpio as wgpio
 from pyft4222.wrapper.common import uninitialize
 
-VERSION: Final = "0.1.2"
+VERSION: Final = "0.2.0"
 
 _DEFAULT_GPIO_DIRS: Final[wgpio.DirTuple] = (
     wgpio.Direction.INPUT,
